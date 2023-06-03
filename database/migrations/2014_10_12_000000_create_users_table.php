58d009b7e84d551955c28c091524a6e5505d78d3
yamisora-sys <20520930@ms.uit.edu.vn>
2023-06-03 13:38:23 +0700

    auth ui & add bootstrap

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fullname');
+            $table->string('CMND/CCCD')->unique();
+            $table->string('phone')->unique();
+            $table->string('address');
+            $table->string('birthday');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
