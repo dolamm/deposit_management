dda6bc7576cc357ea50b78fb91c1e20e2b5c5087
yamisora-sys <20520930@ms.uit.edu.vn>
2023-06-17 13:25:27 +0700

    bao mat thong tin

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,6 +48,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'cmnd_cccd' => 'encrypted',
     ];
 
     public function userRole() {
